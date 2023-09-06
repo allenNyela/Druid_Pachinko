7cab31a12e279d8d6b667e52b83c3e529807a5cb
jack <blazejmg917@gmail.com>
2023-09-05 20:15:02 -0400

    Completed base mechanics
    added scoring, draggable objects, game manager, and ui

---
+++ Druid Pachinko/Assets/Scripts/GameManager.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("the current score")]private int currentScore = 0;
+    [SerializeField, Tooltip("the current money")]private int currentMoney = 0;
+    [SerializeField, Tooltip("the text box for the player's score")]private TMP_Text scoreText;
+    [SerializeField, Tooltip("the text box for the player's money")]private TMP_Text moneyText;
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get{
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject();
+                    _instance = go.AddComponent<GameManager>();
+                    Debug.Log("Generating new game manager");
+                }
+            }
+            return _instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMoney = 0;
+        currentScore = 0;
+        UpdateMoneyText();
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// returns the player's current score
+    /// </summary>
+    /// <returns></returns>
+    public int GetScore(){
+        return currentScore;
+    }
+
+    /// <summary>
+    /// sets the player's score to the given value (does nothing if less than zero)
+    /// </summary>
+    /// <param name="score">the score the player will have</param>
+    public void UpdateScore(int score){
+        if(score < 0){
+            return;
+        }
+        currentScore = score;
+        UpdateScoreText();
+    }
+
+    /// <summary>
+    /// adds the given amount to the player's score
+    /// </summary>
+    /// <param name="score">the amount to increase the score by</param>
+    public void IncreaseScore(int score){
+        currentScore += score;
+        UpdateScoreText();
+    }
+
+    /// <summary>
+    /// returns the player's current money
+    /// </summary>
+    /// <returns></returns>
+    public int GetMoney(){
+        return currentMoney;
+    }
+
+    /// <summary>
+    /// sets the player's money to the given value (does nothing if given less than zero)
+    /// </summary>
+    /// <param name="money">the amount of money the player will have</param>
+    public void UpdateMoney(int money){
+        if(money < 0){
+            return;
+        }
+        currentMoney = money;
+        UpdateMoneyText();
+    }
+
+    /// <summary>
+    /// adds the given amount to the player's money. used when harvesting plants
+    /// </summary>
+    /// <param name="money">the amount of money to add</param>
+    public void AddMoney(int money){
+        currentMoney += money;
+        UpdateMoneyText();
+    }
+
+    /// <summary>
+    /// reduces the player's money by the given amount. used when buying stuff from the shop. cannot reduce money below 0
+    /// </summary>
+    /// <param name="money">the amount of money to subtract from the player</param>
+    public void ReduceMoney(int money){
+        if(money > currentMoney){
+            return;
+        }
+        currentMoney -= money;
+        UpdateMoneyText();
+    }
+
+    /// <summary>
+    /// Checks if the player has more than a certain amount of money. For use in checking if the player can afford shop items.
+    /// </summary>
+    /// <param name="moneyVal">the amount of money to compare to the player's money</param>
+    /// <returns>true if the player's money is greater than or equal to moneyVal</returns>
+    public bool HasEnoughMoney(int moneyVal){
+        return currentMoney >= moneyVal;
+    }
+
+    private void UpdateScoreText(){
+        scoreText.text = "Score: " + currentScore;
+    }
+    private void UpdateMoneyText(){
+        scoreText.text = "Money: " + currentMoney;
+    }
+}
